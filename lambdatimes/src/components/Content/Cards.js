11240cce9fc1cb6ba4330b91eeadde79b8b4f8fa
stevekutz <stkutz@gmail.com>
2019-03-23 12:59:25 -0500

    Card rendering with propTypes completed

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 const Cards = props => {
+  console.log('CARDS props  ', props);
+
+
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+
+      {props.cards.map( (card, i) => {
+        return (
+          <Card
+           key = {i}
+           card = {card}
+          />
+        )
+      })}
+
+
+
     </div>
   )
-}
+};
 
 // Make sure you include prop types for all of your incoming props
 
